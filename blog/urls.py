bff11387341e1b3ef463a38afa1f5dd8640fc4ef
akinaybt <abekmambetova364@gmail.com>
2022-10-13 20:12:06 +0600

    new blog news project

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import (
+    my_api_view
+)
+
+urlpatterns = [
+    path('', my_api_view),
+]
---
+++ blog/views.py
@@ -0,0 +1,53 @@
+from .serializers import NewsSerializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import News
+
+
+@api_view(['GET'])
+def my_api_view(request):
+    data = {
+        'text': 'Hello world !'
+    }
+    return Response(data)
+
+
+@api_view(['POST'])
+def news_update_view(request, id):
+    news = News.objects.get(id=id)
+    serializer = NewsSerializers(instance=news, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def newslist_view(request):
+    tasks = News.objects.all()
+    serializer = NewsSerializers(tasks, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def detail_list_view(request, id):
+    detail_list = News.objects.get(id=id)
+    serializer = NewsSerializers(detail_list, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def news_create_view(request):
+    serializer = NewsSerializers.get(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response({"Created": "Object is created"})
+
+
+@api_view(['DELETE'])
+def task_delete_view(request, id):
+    news = News.objects.get(id=id)
+
+    news.delete()
+    return Response({"obj": "Delete"})
