f51bcac8a9e27c76050c90528d371a5a497f4aba
akinaybt <abekmambetova364@gmail.com>
2022-10-15 19:58:49 +0600

    changes in urls file

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import (
-    my_api_view
+    news_update_view,
+    newslist_view,
+    detail_list_view,
+    news_create_view,
+    task_delete_view,
 )
 
 urlpatterns = [
-    path('', my_api_view),
+    path('news-update/<int:id>', news_update_view),
+    path('news-list/', newslist_view),
+    path('news-detail/<int:id>', detail_list_view),
+    path('news-create/',news_create_view),
+    path('news-delete/<int:id>', task_delete_view),
 ]
